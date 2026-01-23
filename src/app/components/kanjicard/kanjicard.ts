e3ac1a6b88ab638b5826e81807494858eb0f352c
SirRicharod <riccardo.contact@proton.me>
2026-01-23 20:31:39 +0100

    Add kanji card component with styling and input binding for kanji display

--- src/app/components/kanjicard/kanjicard.ts
+++ src/app/components/kanjicard/kanjicard.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-kanjicard',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './kanjicard.css',
 })
 export class Kanjicard {
-
+  @Input() kanji :string = '';
 }
