7634b318991604cd20c3b14137b7278abba1ab4b
SirRicharod <riccardo.contact@proton.me>
2026-01-23 20:26:55 +0100

    Implement explore page with kanji fetching functionality and update app structure for routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home, Footer],
+  imports: [Header, Home, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/pages/explore/explore.ts
+++ src/app/pages/explore/explore.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Kanjiapi } from '../../services/kanjiapi';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-explore',
@@ -6,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './explore.html',
   styleUrl: './explore.css',
 })
-export class Explore {
+
+export class Explore implements OnInit {
+  kanjiList = signal<string[]>([]);
+
+  constructor(
+    private route: ActivatedRoute,
+    private kanjiService: Kanjiapi
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const collection = params['collection']; // This gets 'jlpt-1', 'joyo', etc.
+
+      if (collection) {
+        this.kanjiService.getKanjiByCollection(collection).subscribe({
+          next: (data) => {
+            this.kanjiList.set(data); // Store the array in your signal
+            console.log('Successfully fetched kanji:', data);
+          },
+          error: (err) => {
+            console.error('Error fetching kanji:', err);
+          }
+        });
+      }
+    });
+  }
 
 }
