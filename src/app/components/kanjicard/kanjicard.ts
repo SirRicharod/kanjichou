dcd38f73d184595011c3296539a060b606149d39
SirRicharod <riccardo.contact@proton.me>
2026-01-23 19:11:07 +0100

    Add kanjicard, kanjigrid, and sidepanel components with basic structure

---
+++ src/app/components/kanjicard/kanjicard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanjicard',
+  imports: [],
+  templateUrl: './kanjicard.html',
+  styleUrl: './kanjicard.css',
+})
+export class Kanjicard {
+
+}
---
+++ src/app/components/kanjigrid/kanjigrid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanjigrid',
+  imports: [],
+  templateUrl: './kanjigrid.html',
+  styleUrl: './kanjigrid.css',
+})
+export class Kanjigrid {
+
+}
